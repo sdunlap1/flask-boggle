81c3243a55894925ead50d047c25a1ea2e0448d4
sdunlap1 <sdunlap1@mac.com>
2024-05-01 21:09:29 -0700

    Added starter code to get the board displayed and server to run

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask, request, session, render_template, redirect, jsonify
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'development'
+
 boggle_game = Boggle()
+
+@app.route('/')
+def homepage():
+  """Display the board"""
+  board = boggle_game.make_board()
+  session['board'] = board
+  return render_template('index.html', board=board)
+
+if __name__ == '__main__':
+  app.run(debug=True)
